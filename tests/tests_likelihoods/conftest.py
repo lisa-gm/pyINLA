9372ee233f58472a0b8d724b8baa9ee224fc4a23
Lisa Gaedke <lisagaedke@gmail.com>
2024-09-27 17:28:03 +0200

    added tests all gaussian likelihood functions

--- tests/tests_likelihoods/conftest.py
+++ tests/tests_likelihoods/conftest.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pytest
+from scipy import sparse
 
 N_OBSERVATIONS = [1, 2, 3, 9]
 N_LATENT_PARAMETERS = [1, 2, 3, 8]
@@ -18,3 +20,18 @@ def n_latent_parameters(request):
 @pytest.fixture(params=THETA_OBSERVATIONS, autouse=True)
 def theta_observations(request):
     return request.param
+
+
+@pytest.fixture(scope="function", autouse=False)
+def generate_gaussian_data(
+    n_observations: int, n_latent_parameters: int, theta_observations: float
+):
+    theta_likelihood: dict = {"theta_observations": theta_observations}
+
+    y = np.random.randn(n_observations)
+    a = sparse.random(n_observations, n_latent_parameters, density=0.5)
+    # generate x from a gaussian distribution of dimensions n_latent_parameters with mean 0 and precision exp(theta_observations)
+    variance = 1 / np.exp(theta_observations)
+    x = np.random.normal(loc=0.0, scale=np.sqrt(variance), size=n_latent_parameters)
+
+    return a, x, y, theta_likelihood
---
+++ tests/tests_likelihoods/test_gaussian_likelihood.py
@@ -0,0 +1,72 @@
+import numpy as np
+from autograd import grad, hessian
+from scipy.stats import multivariate_normal
+
+from pyinla.likelihoods.gaussian import GaussianLikelihood
+
+
+def test_gaussian_evaluate_likelihood(
+    generate_gaussian_data,
+    n_observations: int,
+    theta_observations: float,
+    pyinla_config,
+):
+    a, x, y, theta_likelihood = generate_gaussian_data
+    likelihood_instance = GaussianLikelihood(pyinla_config, n_observations)
+
+    eta = a @ x
+    likelihood_inla = likelihood_instance.evaluate_likelihood(y, eta, theta_likelihood)
+
+    # Reference using scipy.stats.multivariate_normal
+    Sigma = 1 / np.exp(theta_observations) * np.eye(n_observations)
+    multivariate_normal_ref = multivariate_normal.logpdf(y, mean=eta, cov=Sigma)
+    # Deduct constant
+    multivariate_normal_ref += 0.5 * n_observations * np.log(2 * np.pi)
+
+    assert np.allclose(likelihood_inla, multivariate_normal_ref)
+
+
+def test_gaussian_evaluate_gradient(
+    generate_gaussian_data,
+    n_observations: int,
+    n_latent_parameters: int,
+    theta_observations: float,
+    pyinla_config,
+):
+    a, x, y, theta_likelihood = generate_gaussian_data
+    eta = a @ x
+
+    likelihood_instance = GaussianLikelihood(pyinla_config, n_observations)
+
+    grad_likelihood_inla = likelihood_instance.evaluate_gradient_likelihood(
+        y, eta, theta_likelihood
+    )
+
+    auto_grad = grad(likelihood_instance.evaluate_likelihood, 1)
+    grad_likelihood_ref = auto_grad(y, eta, theta_likelihood)
+
+    # assert np.allclose(2, 2)
+    assert np.allclose(grad_likelihood_inla, grad_likelihood_ref)
+
+
+def test_gaussian_evaluate_hessian(
+    generate_gaussian_data,
+    n_observations: int,
+    n_latent_parameters: int,
+    theta_observations: float,
+    pyinla_config,
+):
+    a, x, y, theta_likelihood = generate_gaussian_data
+    eta = a @ x
+
+    likelihood_instance = GaussianLikelihood(pyinla_config, n_observations)
+
+    hessian_likelihood_inla = likelihood_instance.evaluate_hessian_likelihood(
+        y, eta, theta_likelihood
+    )
+    hessian_likelihood_inla = hessian_likelihood_inla.toarray()
+
+    auto_hessian = hessian(likelihood_instance.evaluate_likelihood, 1)
+    hessian_likelihood_ref = auto_hessian(y, eta, theta_likelihood)
+
+    assert np.allclose(hessian_likelihood_inla, hessian_likelihood_ref)
