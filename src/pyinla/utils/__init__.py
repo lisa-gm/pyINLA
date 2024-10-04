13244cc8690d2e89bd970b53766bb649c4e557ba
Lisa Gaedke-Merzhaeuser <ihpc060h@alex2.nhr.fau.de>
2024-10-04 10:05:58 +0200

    fixed __init__ utils

--- src/pyinla/utils/__init__.py
+++ src/pyinla/utils/__init__.py
@@ -1,11 +1,11 @@
 # Copyright 2024 pyINLA authors. All rights reserved.
 
-# from pyinla.utils.finite_difference_stencils import (
-#     gradient_finite_difference_3pt,
-#     gradient_finite_difference_5pt,
-#     hessian_diag_finite_difference_3pt,
-#     hessian_diag_finite_difference_5pt,
-# )
+from pyinla.utils.finite_difference_stencils import (
+    gradient_finite_difference_3pt,
+    gradient_finite_difference_5pt,
+    hessian_diag_finite_difference_3pt,
+    hessian_diag_finite_difference_5pt,
+)
 from pyinla.utils.link_functions import sigmoid
 from pyinla.utils.theta_utils import theta_array2dict, theta_dict2array
 
@@ -13,8 +13,8 @@ __all__ = [
     "theta_dict2array",
     "theta_array2dict",
     "sigmoid",
-    # "gradient_finite_difference_3pt",
-    # "gradient_finite_difference_5pt",
-    # "hessian_diag_finite_difference_3pt",
-    # "hessian_diag_finite_difference_5pt",
+    "gradient_finite_difference_3pt",
+    "gradient_finite_difference_5pt",
+    "hessian_diag_finite_difference_3pt",
+    "hessian_diag_finite_difference_5pt",
 ]
