027b947d93553d9817e90a57ed9800fb741dd33f
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-11-05 15:51:54 +0100

    added gitattributes for git-lfs

--- examples/gaussian_regression_example/sandbox_gaussian_regression.py
+++ examples/gaussian_regression_example/sandbox_gaussian_regression.py
@@ -1,35 +1,3 @@
-import os
-
-import numpy as np
-
-from pyinla.core.inla import INLA
-from pyinla.core.pyinla_config import parse_config
-from pyinla.utils.other_utils import print_mpi
-
-path = os.path.dirname(__file__)
-
-################# regression model - gaussian likelihood ##################
-
-if __name__ == "__main__":
-    config = parse_config(f"{path}/config.toml")
-    print("The work directory is:", path)
-
-    pyinla = INLA(config)
-
-    # tic = time.perf_counter()
-    f_value = pyinla.run()
-    # toc = time.perf_counter()
-
-    # print("Initial f:", f_init)
-
-    theta_final = pyinla.theta
-    print_mpi("Final theta:", theta_final)
-
-    x_final = pyinla.x
-    print_mpi("Final x:", x_final)
-
-    os.makedirs(f"{path}/outputs", exist_ok=True)
-
-    np.save(f"{path}/outputs/f_value_pyinla.npy", f_value)
-    np.save(f"{path}/outputs/theta_mean_pyinla.npy", theta_final)
-    np.save(f"{path}/outputs/x_mean_pyinla.npy", x_final)
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b6afa3c6360c8369123cf7bf27227e453c8b1dbf36c3354b9876555a59b58a1
+size 916
