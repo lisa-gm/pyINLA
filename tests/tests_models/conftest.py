a9e26c6a2298ae69f71e20a0e1d15ffa5c34a243
Lisa Gaedke-Merzhaeuser <ihpc060h@alex2.nhr.fau.de>
2024-11-05 13:36:02 +0100

    add forgotten conftest

---
+++ tests/tests_models/conftest.py
@@ -0,0 +1,23 @@
+# Copyright 2024 pyINLA authors. All rights reserved.
+
+import pytest
+
+from pyinla.core.pyinla_config import PyinlaConfig
+
+# import numpy as np
+# from numpy.typing import ArrayLike
+# from scipy import sparse
+
+# from pyinla.core.model import Model
+# from pyinla.models.spatio_temporal import SpatioTemporalModel
+
+
+@pytest.fixture(scope="function", autouse=False)
+def pyinla_config_model():
+    pyinla_config = PyinlaConfig()
+
+    pyinla_config.model.theta_spatial_range = 1.0
+    pyinla_config.model.theta_temporal_range = 1.0
+    pyinla_config.model.theta_spatio_temporal_variation = 1.0
+
+    return pyinla_config
