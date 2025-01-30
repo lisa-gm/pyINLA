1f2bc89717994e87b605295b68bcd5cb79c04c59
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2025-01-30 14:30:08 +0100

    Modified example inputs

---
+++ examples/gst_medium/run.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:782514c076ca19f95c3821c4e0126ea7b61ef03f19b3680e627b37b37cd9edb0
+size 1942
--- examples/gst_small/run.py
+++ examples/gst_small/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:541baaa7ea6aa17636b035b4a60e68b23e61f126d705e1948ff8af6b7fe45629
+oid sha256:52a9e4da8f771a8e04e3e3deb8c79bffaaa156b3a68ee4c70637b29b4977edbc
 size 1958
