d0de9a2b727338e738f50ce423027b355d093c2c
Lisa Gaedke <lisagaedke@gmail.com>
2025-02-18 14:28:49 +0300

    updated default parameters

--- examples/brainiac/run.py
+++ examples/brainiac/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:584e4a8e329e06df87c2488aa8481771ea7cc6dc2f1f4b041f1729ebcb6acee2
-size 1608
+oid sha256:6f186af48a6a64f795e645a52522e47a0ac89a9ae66f7ec3b899d4803e8674ad
+size 1656
