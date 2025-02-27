6ddaf3301e43552cb617da8764c44cd81d3a57e9
Lisa Gaedke <lisagaedke@gmail.com>
2025-02-27 12:49:17 +0300

    minor stuff

--- examples/gst_small/run.py
+++ examples/gst_small/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a2f7235e4b26405692594eaf60ed7c528bcfc376e46db789547e5df40135ebe
-size 2883
+oid sha256:1bb2079d69754eb8f1fd5d2470ebe10e4f896124cbb6f62cea5f60c1a07e7272
+size 3186
