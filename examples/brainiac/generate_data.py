daeb17e2838dfb7668e61d1ef56247d6fd5e55cf
Lisa <lisa@kl-26644.local>
2025-03-17 10:46:25 +0300

    remains unresolved scaling issue of a

--- examples/brainiac/generate_data.py
+++ examples/brainiac/generate_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e76ba9e6443c1d66607e58d161ffeda90b180eb75bc1cec782ad78091f8e66d
-size 2177
+oid sha256:9041d23f0524f043efc1031606c0f107a28a8b8b78c25c473fd4594d8921ab8b
+size 3238
