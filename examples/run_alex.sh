8dbe313c4a0b4741f0f15b6cb48189b7aec313c3
lisa-gm <lisagaedke@gmail.com>
2025-04-02 16:05:27 +0200

    udpated run script

--- examples/run_alex.sh
+++ examples/run_alex.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e3765d980bbe2d77ef8352bbaee33f57b0b265bcd8193cbf9706c641c2f548
-size 835
+oid sha256:736ea580c546942e148f9c53667f86a561eb46bda707053e5c582f642a4a58d6
+size 928
