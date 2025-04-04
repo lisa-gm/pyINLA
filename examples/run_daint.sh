02900567c420bddedf6cf5723eb75d0ab584ec12
Lisa Gaedke-Merzhaeuser <lgaedkem@daint-ln004.cscs.ch>
2025-04-04 17:04:02 +0200

    testing tiling approach

--- examples/run_daint.sh
+++ examples/run_daint.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0cb399e8875711e790d5fc6ace52a488843acc184043a8de7692875e9376b63
-size 1129
+oid sha256:99f6a178e2299c0da01777a085aa527fc5e1d8a7067c52910f6f5d8691ddaa40
+size 1172
