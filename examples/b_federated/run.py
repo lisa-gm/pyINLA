4cc0cf4ec3941b16a15046e70049167bd264e7d4
Lisa Gaedke <lisagaedke@gmail.com>
2026-05-02 16:52:57 +0300

    added examples of complete dataset for federated inference. currently just running all together

---
+++ examples/b_federated/run.py
@@ -0,0 +1,153 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+from dalia import xp
+from dalia.configs import dalia_config, likelihood_config, submodels_config
+from dalia.core.dalia import DALIA
+from dalia.core.model import Model
+from dalia.submodels import RegressionSubModel
+from dalia.utils import print_msg
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+from examples_utils.parser_utils import parse_args  # noqa: E402
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    print_msg("--- Example: Gaussian Regression ---")
+
+    # Check for parsed parameters
+    args = parse_args()
+
+    data_type = "trauma"
+    family = "binomial"
+
+    # Configurations of the regression submodel
+    regression_dict = {
+        "type": "regression",
+        "input_dir": f"{BASE_DIR}/{data_type}_{family}",
+        "n_fixed_effects": 5,
+        "fixed_effects_prior_precision": 0.001,
+    }
+    regression = RegressionSubModel(
+        config=submodels_config.parse_config(regression_dict),
+    )
+
+    # Likelihood
+    likelihood_dict = {
+        "type": "binomial",
+        "input_dir": f"{BASE_DIR}/{data_type}_{family}",
+    }
+    # Creation of the first model by combining the Regression submodel and the likelihood
+    model = Model(
+        submodels=[regression],
+        likelihood_config=likelihood_config.parse_config(likelihood_dict),
+    )
+    print_msg(model)
+
+    # Configurations of DALIA
+    dalia_dict = {
+        "solver": {"type": "dense"},
+        "minimize": {
+            "max_iter": args.max_iter,
+            "gtol": 1e-3,
+            "disp": True,
+        },
+        "inner_iteration_max_iter": 50,
+        "eps_inner_iteration": 1e-3,
+        "eps_gradient_f": 1e-3,
+        "simulation_dir": ".",
+    }
+    dalia = DALIA(
+        model=model,
+        config=dalia_config.parse_config(dalia_dict),
+    )
+
+    # theta_ref = xp.load(f"{BASE_DIR}/reference_outputs/theta_ref.npy")
+    # x_ref = xp.load(f"{BASE_DIR}/reference_outputs/x_ref.npy")
+
+    results = dalia.run()
+
+    print_msg("\n--- Results ---")
+    fixed_effects_mean = results["x"][-model.submodels[-1].n_fixed_effects :]
+    print_msg(
+        "Mean of the fixed effects:\n",
+        fixed_effects_mean,
+    )
+
+    # print_msg("\n--- Comparisons ---")
+    # # Compare hyperparameters
+    # theta_ref = xp.load(f"{BASE_DIR}/reference_outputs/theta_ref.npy")
+    # print_msg("Reference theta:", theta_ref)
+    # print_msg(
+    #     "Norm (theta - theta_ref):        ",
+    #     f"{xp.linalg.norm(results['theta'] - theta_ref):.4e}",
+    # )
+
+    # # Compare latent parameters
+    # x_ref = xp.load(f"{BASE_DIR}/reference_outputs/x_ref.npy")
+    # print_msg(
+    #     "Norm (x - x_ref):                ",
+    #     f"{xp.linalg.norm(results['x'] - x_ref):.4e}",
+    # )
+
+    # # Compare marginal variances of latent parameters
+    var_latent_params = results["marginal_variances_latent"]
+    fixed_effects_var = var_latent_params[-model.submodels[-1].n_fixed_effects :]
+    fixed_effects_sd = np.sqrt(fixed_effects_var)
+    ci_lower = fixed_effects_mean - 1.96 * fixed_effects_sd
+    ci_upper = fixed_effects_mean + 1.96 * fixed_effects_sd
+
+    print_msg("95% credible intervals of fixed effects (from marginal variances):")
+    for i, (mean_i, low_i, up_i) in enumerate(
+        zip(fixed_effects_mean, ci_lower, ci_upper), start=1
+    ):
+        print_msg(f"  x[{i}] mean={mean_i:.6f}, CI95=[{low_i:.6f}, {up_i:.6f}]")
+
+    # Qconditional = dalia.model.construct_Q_conditional(eta=model.a @ model.x)
+    # Qinv_ref = xp.linalg.inv(Qconditional.toarray())
+    # print_msg(
+    #     "Norm (marg var latent - ref):    ",
+    #     f"{np.linalg.norm(var_latent_params - xp.diag(Qinv_ref)):.4e}",
+    # )
+
+    # # Compare marginal variances of observations
+    # var_obs = dalia.get_marginal_variances_observations(
+    #     theta_external=theta_ref, x_star=x_ref
+    # )
+    # var_obs_ref = extract_diagonal(model.a @ Qinv_ref @ model.a.T)
+    # print_msg(
+    #     "Norm (var_obs - var_obs_ref):    ",
+    #     f"{xp.linalg.norm(var_obs - var_obs_ref):.4e}",
+    # )
+
+    # store parameters in matching format as needed in R
+    # make dataframe with columns:
+    # - lower (2.5% quantile)
+    # - upper (97.5% quantile)
+    # - mean (mean of the fixed effect)
+    # - Method (DALIA)
+    # - covariate (intercept), gender, age, ISS, GCS
+
+    df_dalia = pd.DataFrame(
+        {
+            "lower": ci_lower,
+            "upper": ci_upper,
+            "Estimate": fixed_effects_mean,
+            "Method": "DALIA",
+            "Covariate": [
+                "(Intercept)",
+                "sex",
+                "age",
+                "ISS",
+                "GCS",
+            ],
+        }
+    )
+    df_dalia.to_csv(f"{BASE_DIR}/dalia_summary_{data_type}.csv", index=False)
+
+    print_msg("\n--- Finished ---")
